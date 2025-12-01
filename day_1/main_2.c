b1fe1ceff1b95dfafe05af66b092c08fa9cb2791
Maya (engired) <engineredoneac77@gmail.com>
2025-12-01 18:06:53 +1100

    final solution for day 1! (ffs it was ONE LINE and I moved it for ZERO reason and it gave me the wrong answer (which I knew it would)). (I should've gotten this done like 15 minutes earlier)

--- day_1/main_2.c
+++ day_1/main_2.c
@@ -3,19 +3,32 @@
 
 const int begin = 50;
 
+/*
+82, 1
+52, 1
+0, 2
+95, 2
+55, 3
+0, 4
+99, 4
+0, 5
+14, 5
+32, 6
+*/
+
 int main()
 {
 	FILE* fptr;
 	size_t flength;
-	int dial = begin, dir = 1, ret = 0;
+	int dial = begin, dir = 1, ret = 0, oldret = 0;
 	char* buffer = (char*)malloc(32);
 	char* nums = (char*)malloc(32);
 	
-	fptr = fopen("test", "r");
+	fptr = fopen("input", "r");
 	if(fptr == NULL)
 		exit(EXIT_FAILURE);
 
-	while(fgets(buffer, 32, fptr))
+	while(fgets(buffer, 32, fptr) )
 	{
 		dir = buffer[0] == 'R' ? 1 : -1;
 
@@ -29,11 +42,27 @@ int main()
 		nums[i - 1] = '\0';
 
 		dial += atoi(nums) * dir;
-		while(dial < 0)
-			dial = 100 + dial;
-		dial %= 100;
 
-		ret += (dial == 0);
+		// if it was previously zero, then don't count this tick
+		if(dial < 0 && dial - (atoi(nums) * dir) == 0)
+			ret--;
+
+		// if it's less than 0, it has to pass zero once and then
+		// same logic while it is still less than zero
+		while(dial < 0)
+		{
+			dial = 100 + dial;
+			++ret;
+		}
+
+		// if it points directly to zero
+		ret += dial == 0;
+
+		// add for each 100 rotation
+		ret += dial / 100;
+		dial %= 100;		
+
+		oldret = ret;
 	}
 
 	fclose(fptr);
